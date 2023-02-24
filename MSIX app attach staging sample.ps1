f9c9208411adfce325156ea0c12259c859a53f25
HungryBoySL <dkmcw20@users.noreply.github.com>
2023-02-24 14:48:26 +1100

    Update MSIX app attach staging sample.ps1

--- MSIX app attach staging sample.ps1
+++ MSIX app attach staging sample.ps1
@@ -1,46 +1,82 @@
-#MSIX app attach staging sample
+<#
+.SYNOPSIS
+This script installs a list of applications on a Windows 10 machine using the MSIX format.
 
-#region variables
-$vhdSrc="\\dckloudmain.file.core.windows.net\appattach\vscode2.vhd"
-$packageName = "vscode2.vhd"
-$parentFolder = "vscode"
-$parentFolder = "\" + $parentFolder + "\"
-$volumeGuid = "c525203c-351d-42ae-bd5c-a2a65c070cfc"
+.DESCRIPTION
+This script installs a list of applications on a Windows 10 machine using the MSIX format. The list of applications to be installed should be specified in a JSON file with the following format:
+[
+    {
+        "Name": "Application 1",
+        "Path": "\\server\share\Application1.msix"
+    },
+    {
+        "Name": "Application 2",
+        "Path": "\\server\share\Application2.msix"
+    },
+    ...
+]
+
+The script will mount each MSIX package as a virtual disk, stage it for installation, and then uninstall the virtual disk.
+
+.PARAMETER JsonFilePath
+The path to the JSON file containing the list of applications to be installed.
+
+.EXAMPLE
+Install-MSIXApps -JsonFilePath "C:\Applications.json"
+
+.NOTES
+Author: Your Name
+Date:   2023-02-23
+#>
+
+# Define the input parameter
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$JsonFilePath
+)
+
+# Import the JSON file containing the list of applications to be installed
+$Apps = Get-Content -Path $JsonFilePath -Raw | ConvertFrom-Json
+
+# Define the variables
 $msixJunction = "C:\temp\AppAttach\"
-#endregion
+$volumeGuid = "c525203c-351d-42ae-bd5c-a2a65c070cfc"
 
-#region mountvhd
-try
-{
-      Mount-Diskimage -ImagePath $vhdSrc -NoDriveLetter -Access ReadOnly
-      Write-Host ("Mounting of " + $vhdSrc + " was completed!") -BackgroundColor Green
-}
-catch
-{
-      Write-Host ("Mounting of " + $vhdSrc + " has failed!") -BackgroundColor Red
-}
-#endregion
+# Loop through the list of applications and install each one
+foreach ($App in $Apps) {
+    $packageName = $App.Name
+    $packagePath = $App.Path
 
-#region makelink
-$msixDest = "\\?\Volume{" + $volumeGuid + "}\"
-if (!(Test-Path $msixJunction))
-{
-     md $msixJunction
-}
+    # Mount the MSIX package as a virtual disk
+    try {
+        Mount-DiskImage -ImagePath $packagePath -NoDriveLetter -Access ReadOnly
+        Write-Host "Mounting of $packageName was completed!" -BackgroundColor Green
+    }
+    catch {
+        Write-Host "Mounting of $packageName has failed!" -BackgroundColor Red
+    }
 
-$msixJunction = $msixJunction + $packageName
-cmd.exe /c mklink /j $msixJunction $msixDest
-#endregion
+    # Create a junction point for the virtual disk
+    $msixDest = "\\?\Volume{$volumeGuid}\"
+    if (!(Test-Path $msixJunction)) {
+        md $msixJunction
+    }
 
-#region stage
-[Windows.Management.Deployment.PackageManager,Windows.Management.Deployment,ContentType=WindowsRuntime] | Out-Null
-Add-Type -AssemblyName System.Runtime.WindowsRuntime
-$asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where { $_.ToString() -eq 'System.Threading.Tasks.Task`1[TResult] AsTask[TResult,TProgress](Windows.Foundation.IAsyncOperationWithProgress`2[TResult,TProgress])'})[0]
-$asTaskAsyncOperation = $asTask.MakeGenericMethod([Windows.Management.Deployment.DeploymentResult], [Windows.Management.Deployment.DeploymentProgress])
-$packageManager = [Windows.Management.Deployment.PackageManager]::new()
-$path = $msixJunction + $parentFolder + $packageName 
-$path = ([System.Uri]$path).AbsoluteUri
-$asyncOperation = $packageManager.StagePackageAsync($path, $null, "none")
-$task = $asTaskAsyncOperation.Invoke($null, @($asyncOperation))
-$task
-#endregion
+    $msixJunction = $msixJunction + $packageName
+    cmd.exe /c mklink /j $msixJunction $msixDest
+
+    # Stage the application for installation
+    [Windows.Management.Deployment.PackageManager,Windows.Management.Deployment,ContentType=WindowsRuntime] | Out-Null
+    Add-Type -AssemblyName System.Runtime.WindowsRuntime
+    $asTask = ([System.WindowsRuntimeSystemExtensions].GetMethods() | Where { $_.ToString() -eq 'System.Threading.Tasks.Task`1[TResult] AsTask[TResult,TProgress](Windows.Foundation.IAsyncOperationWithProgress`2[TResult,TProgress])'})[0]
+    $asTaskAsyncOperation = $asTask.MakeGenericMethod([Windows.Management.Deployment.DeploymentResult], [Windows.Management.Deployment.DeploymentProgress])
+    $packageManager = [Windows.Management.Deployment.PackageManager]::new()
+    $path = $msixJunction + "\" + $packageName 
+    $path = ([System.Uri]$path).AbsoluteUri
+    $asyncOperation = $packageManager.StagePackageAsync($path, $null, "none")
+    $task = $asTaskAsyncOperation.Invoke($null, @($asyncOperation))
+    $task
+
+    # Unmount the virtual disk
+    $diskImage = Get-DiskImage -ImagePath $
